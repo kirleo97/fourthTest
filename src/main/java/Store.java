c52ed8705b4f03d5888957505567e6e8cd3a0933
kleonov <wade.97@mail.ru>
2019-12-09 13:58:52 +0300

    upload

--- src/main/java/Store.java
+++ src/main/java/Store.java
@@ -1,32 +1,61 @@
 
 public class Store {
-    private static Store store;
+    private static final Store store = new Store();
     private volatile int goods = 1000;
     private int maxPurchases;
 
     private Store() {
     }
 
-    public static Store getInstance() {
-        if (store == null)
-            return new Store();
-        else
-            return store;
-    }
+    public static Store getInstance() { return store; }
 
     public int getGoods() {
         return goods;
     }
 
-    public int getMaxPurchases() {
-        return maxPurchases;
+    public int getMaxPurchases() { return maxPurchases; }
+
+    public void setMaxPurchases(int maxPurchases) { this.maxPurchases = maxPurchases; }
+
+    public void setGoods(int goods) { this.goods = goods; }
+
+
+    public void tryToBuy(Consumer consumer, int quantity) {
+        if (checkTaskCondition(consumer)) {
+            int finalQuantity = checkBalanceOfGoods(quantity);
+            changeConsumerDataAfterDeal(consumer, finalQuantity);
+            setGoods(getGoods() - finalQuantity);
+            if (consumer.getNumberOfPurchases() >  getMaxPurchases())
+                setMaxPurchases(consumer.getNumberOfPurchases());
+            printDeal(consumer, finalQuantity);
+        }
     }
 
-    public void setMaxPurchases(int maxPurchases) {
-        this.maxPurchases = maxPurchases;
+    public boolean checkTaskCondition(Consumer consumer) {
+        if (consumer.getNumberOfPurchases() - Store.getInstance().getMaxPurchases() > 0) {
+            System.out.printf("Потребитель %d не может совершить новую покупку. Система не позволяет\n", consumer.getName());
+            return false;
+        }
+        return true;
     }
 
-    public void setGoods(int goods) {
-        this.goods = goods;
+    public int checkBalanceOfGoods(int quantity) {
+        int balanceOfGoods = getGoods();
+        if (quantity > balanceOfGoods) {
+            System.out.printf("Количество требуемого товара меньше остатка на складе. Вы можете купить только %d товаров(а)\n", balanceOfGoods);
+            return balanceOfGoods;
+        }
+        return quantity;
+    }
+
+    public void changeConsumerDataAfterDeal(Consumer consumer, int goods) {
+        consumer.setNumberOfGoods(consumer.getNumberOfGoods() + goods);
+        consumer.setNumberOfPurchases(consumer.getNumberOfPurchases() + 1);
+    }
+
+    public void printDeal(Consumer consumer, int quantity) {
+        System.out.println("Потребитель " + consumer.getName() + " купил " + quantity + " товаров(а);");
+        System.out.println("Сейчас у него " + consumer.getNumberOfGoods() + " товаров(а) и " + consumer.getNumberOfPurchases() + " покупок(и);");
+        System.out.println("В магазине осталось " + getGoods() + " товаров(а), а максимальное число покупок = " + getMaxPurchases() + "\n");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,15 +1,7 @@
 
 public class Task {
     public static void main(String[] args) {
-        Store store = Store.getInstance();
-        for (int i = 1; i < 11; i++) {
-            new Consumer(i).getThread().start();
-        }
-        while (true) {
-            if (store.getGoods() == 0) {
-                System.out.println("Конец циклов");
-                break;
-            }
-        }
+        for (int i = 1; i < 11; i++)
+            new Thread(new Consumer(i)).start();
     }
 }
